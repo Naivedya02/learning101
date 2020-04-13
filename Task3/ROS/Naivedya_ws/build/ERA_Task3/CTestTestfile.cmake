74d286b52873c367805b5636970769a5f0e5ef3b
Naivedya <naivedya@iitk.ac.in>
2020-04-13 19:55:30 +0530

    Add existing file

---
+++ Task3/ROS/Naivedya_ws/build/ERA_Task3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/naivedya/Naivedya_ws/src/ERA_Task3
+# Build directory: /home/naivedya/Naivedya_ws/build/ERA_Task3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Task3/ROS/Naivedya_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/naivedya/Naivedya_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/naivedya/Naivedya_ws/devel;/opt/ros/kinetic".split(';') if "/home/naivedya/Naivedya_ws/devel;/opt/ros/kinetic" != "" else []
